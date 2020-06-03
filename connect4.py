2714f6e4a97013f0269597d9d68b757b0ceb69d7
obiwankubernetes <samrperl@gmail.com>
2020-06-03 12:00:38 -0400

    fin part 4, need pygame pack

--- connect4.py
+++ connect4.py
@@ -4,10 +4,14 @@
 ### load packages
 import numpy as np
 import sys
-# import pygame
+import pygame
 import math
 
 ### global variables
+BLUE = (0,0,255)
+BLACK = (0,0,0)
+RED = (255,0,0)
+YELLOW = (255,255,0)
 ROW_COUNT = 6
 COLUMN_COUNT = 7
 
@@ -72,6 +76,27 @@ def winning_move(board, piece):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 # return true which ends game loop
                 return True
+
+# function to draw board with pygame
+def draw_board(board):
+    # fill in background first
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            # draw big blue rectangle below one solid black row
+            pygame.draw.rect(screen, BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            # draw circles in each row/column position
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    # fill in pieces 2nd based on what players pick
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):      
+            # make player 1 piece red
+            if board[r][c] == 1:
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            # make player 2 piece yellow
+            elif board[r][c] == 2:
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    pygame.display.update()    
+
 ### initialize basic features of game
 # init board 
 board = create_board()
@@ -82,37 +107,97 @@ game_over = False
 # init turn starting at 0 count
 turn = 0
 
+### init pygame and design feature
+pygame.init()
+# init radius for circles
+RADIUS = int(SQUARESIZE/2 - 5)
+# square size 100 pixels
+SQUARESIZE = 100
+# width of display
+width = COLUMN_COUNT * SQUARESIZE
+# height of display
+height = (ROW_COUNT + 1) * SQUARESIZE
+# size of display is a tuple of width and height
+size = (width, height)
+# init screen with pygame package functions
+screen = pygame.display.set_mode(size)
+# draw board
+draw_board(board)
+# update display pygame
+pygame.display.update()
+# init pygame font
+myfont = pygame.font.SysFont("monospace", 75)
+
+
 ### Main loop to operate game
 # create game loop that runs while game is not over
 while not game_over:
-    # Ask for Player 1 input
-    if turn == 0:
-        col = int(input("Player 1 Make Your Selection (0-6):"))
-        # if open col then find next open row and drop piece for player 1
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 1)
-            # if move hits a win, declare player 1 victory
-            if winning_move(board, 1):
-                print("Player 1 Wins !!!")
-                game_over = True
-    # ask player 2 input
-    else:
-        col = int(input("Player 2 Make Your Selection (0-6):"))
-        # if open col then find next open row and drop piece for player 1
-        if is_valid_location(board, col):
-            row = get_next_open_row(board, col)
-            drop_piece(board, row, col, 2)    
-            # if move hits a win, declare player 1 victory
-            if winning_move(board, 2):
-                print("Player 2 Wins !!!")
-                game_over = True
-                # break loop
-                break
+    
+    for event in pygame.event.get():
+        # quit/exit feature
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+        # mouse scroll to see piece while moving mouse on top blank row
+        if event.type == pygame.MOUSEMOTION:
+            # draw black rectangle at top
+            pygame.draw.rect(screen, BLACK, (0,0,width, SQUARESIZE))
+            # position is row 1
+            posx = event.pos[0]
+            # if player one turn -red (based on rem of 0)
+            if turn == 0:
+                pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+            # elese player 2 turn - yellow
+            else:
+                pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+        pygame.display.update()
+
+        # clickdown feature
+        if event.type == pygame.MOUSEBUTTONDOWN:     
+             # draw black rectangle at top
+            pygame.draw.rect(screen, BLACK, (0,0,width, SQUARESIZE))
+            # use pygame to select column based on click
+            # print(event.pos)
+            # Ask for Player 1 input
+            if turn == 0:
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+                # if open col then find next open row and drop piece for player 1
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 1)
+                    # if move hits a win, declare player 1 victory
+                    if winning_move(board, 1):
+                        # init victory label with pygame font
+                        label = myfont.render("player 1 Wins!", 1, RED)
+                        # update screen at coordinate 40,10 (top empty row)
+                        screen.blit(label, (40,10))
+                        game_over = True
             
-    # after player 2 goes then print the board
-    print_board(board)
-    # at end of turn increase turn count by 1
-    turn += 1
-    # determine if odd or even by taking turn divide by two and calc remainder. If no remainder than even and player 1 turn, if remainder, then it must be odd and player 2 turn
-    turn = turn % 2
+            # ask player 2 input
+            else:
+                posx = event.pos[0]
+                col = int(math.floor(posx/SQUARESIZE))
+                # if open col then find next open row and drop piece for player 1
+                if is_valid_location(board, col):
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, 2)    
+                    # if move hits a win, declare player 1 victory
+                    if winning_move(board, 2):
+                        # init victory label with pygame font
+                        label = myfont.render("player 2 Wins!", 1, YELLOW)
+                        # update screen at coordinate 40,10 (top empty row)
+                        screen.blit(label, (40,10))
+                        game_over = True
+                        
+                
+        # after player 2 goes then print the board
+        print_board(board)
+        draw_board(board)
+        # at end of turn increase turn count by 1
+        turn += 1
+        # determine if odd or even by taking turn divide by two and calc remainder. If no remainder than even and player 1 turn, if remainder, then it must be odd and player 2 turn
+        turn = turn % 2
+        # on a win, wait 3 secs to shutdown
+        if game_over:
+            pygame.time.wait(3000)
